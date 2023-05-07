7425ce91d4773450ffa584a09f692513df357af1
Ketema19 <131938061+Ketema19@users.noreply.github.com>
2023-05-07 14:21:32 -0400

    commit from eclipse

---
+++ javasampletsion/src/com/crafttsion/exerciseone.java
@@ -0,0 +1,14 @@
+package com.crafttsion;
+
+public class exerciseone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		System.out.print ("print something");
+		System.out.print ("print something");
+		System.out.print ("print something");
+	}
+
+}
