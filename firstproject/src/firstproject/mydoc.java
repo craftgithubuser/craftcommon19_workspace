2632a0b9e9fcc5eb45bf7a5f4ce32ff4982ca83b
meazaB2023 <meazabizuneh@gmail.com>
2023-05-09 15:56:01 -0400

    Initial from Eclipse

---
+++ firstproject/src/firstproject/mydoc.java
@@ -0,0 +1,14 @@
+package firstproject;
+
+public class mydoc {
+
+public static void main(String[] args) {
+		//TODO Auto-generated method stubs
+	
+	System.out.println("Hello everyone");
+	
+	}
+
+}
+	
+		
