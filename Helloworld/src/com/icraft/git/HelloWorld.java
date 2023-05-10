fb91a056181b4a5669e60b4a908dad740f800041
Mickeydes <mickeydes78@gmail.com>
2023-05-10 04:00:17 -0400

    initial commit Eclipse Mickey

---
+++ Helloworld/src/com/icraft/git/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.icraft.git;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("Hello Team");	
+		
+
+
+}
+	
+}
