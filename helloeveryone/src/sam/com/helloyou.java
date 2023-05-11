fff5c8b28ae0a39636971be2bf72ef80f1db171d
SamerawitMe <131940228+SamerawitMe@users.noreply.github.com>
2023-05-12 00:51:49 +0530

    samcraft23

---
+++ helloeveryone/src/sam/com/helloyou.java
@@ -0,0 +1,10 @@
+package sam.com;
+
+
+public class helloyou {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method  
+	}
+system.out.printin("hello team"));
+}      seysetm.out.printin("hello");
