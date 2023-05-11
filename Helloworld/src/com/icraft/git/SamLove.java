eb3334ad1b8ebde41e5389d0bc8ab2cb4372b381
SamerawitMe <131940228+SamerawitMe@users.noreply.github.com>
2023-05-11 14:52:46 +0530

    I added java class callled "SamLove"

---
+++ Helloworld/src/com/icraft/git/SamLove.java
@@ -0,0 +1,18 @@
+package com.icraft.git;
+
+public class SamLove {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Team craft, how are you?");
+		System.out.println("I am good, How about you?");
+		System.out.println("Hello Team craft, how are you?");
+		System.out.println("Hello Team craft, how are you?");
+		System.out.println("Hello Team craft, how are you?");
+		System.out.println("Hello Team craft, how are you?");
+		
+
+	}
+
+}
