cef9561f43beb4387c534b649992d3012f1b62c2
Lelloo23 <leelloo.d@gmail.com>
2023-05-15 13:34:03 -0400

    commit from eclipse

---
+++ LeloLelo/src/com/lelo/Example1.java
@@ -0,0 +1,18 @@
+package com.lelo;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("print something");
+		System.out.print("print something");
+		System.out.print("print something");
+		System.out.print("print something");
+		System.out.print("print something");
+		System.out.print("print something");
+		
+
+	}
+
+}
