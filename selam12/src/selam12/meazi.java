0f11b894f7f4904a0619426f1dd4a4da9ec6d761
selamawitkiflemariam <Kselamawit7@gmail.come>
2023-05-15 22:19:29 -0700

    I creat project.
    Merge branch 'main' of
    https://github.com/craftgithubuser/craftcommon19_workspace.git into main

--- selam12/src/selam12/meazi.java
+++ selam12/src/selam12/meazi.java
@@ -1,10 +1,11 @@
-package merry;
+package selam12;
 
-public class selam_t {
+public class meazi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("afer mid night");
 	}
 
 }
