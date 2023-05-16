477e4325d947b2017dcee6bf057db3a655bd4481
abelw <abelsislina@yahoo.com>
2023-05-15 20:56:04 -0400

    My first eclipse commit

---
+++ javasampleone/src/sampleone/sample1.java
@@ -0,0 +1,16 @@
+package sampleone;
+
+public class sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("this is my first java 2023");
+        System.out.println("this is my first java 2023");
+        System.out.println("this is my first java 2023");
+        System.out.println("this is my first java 2023");
+        System.out.println("this is my first java 2023");
+        System.out.println("this is my first java 2023");
+        System.out.println("this is my first java 2023");
+	}
+
+}
