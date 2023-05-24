5f098592b56d6e05f11d083165f594bd5bc4d38f
selamawitkiflemariam <Kselamawit7@gmail.come>
2023-05-24 15:53:42 -0700

    create

---
+++ selam12/src/monday/Newclass.java
@@ -0,0 +1,12 @@
+package monday;
+
+public class Newclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Thanks God ");
+       System.out.println("Thanks GOd");
+       System.out.println("Thanks God");
+	}
+
+}
