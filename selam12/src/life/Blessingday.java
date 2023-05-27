e54680c769f0ca68ef628078f5fdc96f12e9d5a5
selamawitkiflemariam <Kselamawit7@gmail.come>
2023-05-27 12:05:54 -0700

    Merge branch 'main' i update from eclipse https://github.com/craftgithubuser/craftcommon19_workspace.git into main

--- selam12/src/life/Blessingday.java
+++ selam12/src/life/Blessingday.java
@@ -5,6 +5,10 @@ public class Blessingday {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+
+
+		System.out.println("God with me");
+		System.out.println("God with me");
 	}
 
 }
