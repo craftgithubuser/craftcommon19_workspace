409fb6333215430e9328bb08c891901283c84a0a
Estifanos99 <estifaynalem99@gmail.com>
2023-06-11 19:26:34 -0400

    June 11 2023

---
+++ lopp4.java
@@ -0,0 +1,19 @@
+
+public class lopp4 {
+ 
+  // write a code that will print a number from 1 to 30
+   public static void main(String args[]){
+  //while loop  
+  // print statement
+  
+  System.out.print("output is :");
+  
+  int i=1; //initi...
+  while(i<=30)
+  {
+	  System.out.println(i);
+  i++;
+}
+}
+}
+ 
