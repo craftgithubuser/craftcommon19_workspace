09442cab692f8edbfa3fe29daf6ab526e4ce2920
selamawitkiflemariam <Kselamawit7@gmail.come>
2023-06-14 08:41:08 -0700

    message

---
+++ craftCommn_19.java/src/assigment_exersise/DesendingOrder.java
@@ -0,0 +1,35 @@
+package assigment_exersise;
+import java.util.Scanner;
+public class DesendingOrder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// getIntegers Enter integer from keybord.
+     Scanner input= new Scanner(System.in);
+   //  System.out.println("pls enter number of size");
+     int size =input.nextInt();
+     
+	int[]getinteger	=new int[size];
+	//System.out.println("pls enter the integer of array");
+     for(int i=0;i<size;i++) {
+			getinteger[i] = input.nextInt();
+		
+      
+     
+     }
+     printArray(getinteger);
+     
+	}
+
+	private static void printArray(int[] getinteger) {
+		// TODO Auto-generated method stub
+		
+		int size =5;
+		for(int i=0;i<size;i++) {
+		int printArra[] = {106,26,81,5,15};
+		System.out.println("Pls enter printArry");
+		
+	}
+
+}
+}
