cdb3c9284057a989c764565255c30d850d58a5be
Ketema19 <131938061+Ketema19@users.noreply.github.com>
2023-06-14 22:28:40 -0400

    new data

---
+++ world1/src/world1exersice/workout.java
@@ -0,0 +1,11 @@
+package world1exersice;
+
+public class workout {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("print semothing");
+	
+	}
+
+}
