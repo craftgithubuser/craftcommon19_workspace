1cdcb8e1b67993d420f021203db42a55c5e0568e
Ketema19 <131938061+Ketema19@users.noreply.github.com>
2023-06-17 17:10:37 -0400

    Add new information.

---
+++ craft_common19_java_class_tsion/src/com/company/CarMain.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class CarMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ craft_common19_java_class_tsion/src/com/loops/WhileLoop.java
@@ -0,0 +1,14 @@
+package com.loops;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=1;
+while(i<=10) {
+	System.out.print(i+" ");
+	i++;
+}
+	}
+
+}
