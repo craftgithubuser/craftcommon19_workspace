5bed0df9adef7d2b688c8538ee4750ca6aa62270
Ketema19 <131938061+Ketema19@users.noreply.github.com>
2023-06-17 16:55:21 -0400

    Add int odd number.

---
+++ javasample2/src/com/exerceoneday/EvenOddCheck.java
@@ -0,0 +1,27 @@
+package com.exerceoneday;
+
+import java.util.Scanner;
+
+public class EvenOddCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner input = new Scanner(System.in);
+do {
+System.out.println("pls Enter any Integer number:");
+int value = input.nextInt();
+String result = (value%2 != 0)? "you entered odd number" : "you entered even number";
+if(value%2 == 0) {
+		result = "you entered odd number";
+}
+else {
+	result = "you entered odd number";
+}
+System.out.println(result);
+System.out.println("pressy/Y to countinue:");
+Cont = input.next{};
+}while(cont. equalIsIgnoreCase) {"Y"}};
+
+	}
+
+
