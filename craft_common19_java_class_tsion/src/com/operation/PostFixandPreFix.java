92a8a27fb3cf61b79e1988d51ece4e6c011fd725
Ketema19 <131938061+Ketema19@users.noreply.github.com>
2023-06-18 22:15:01 -0400

    new exersice.

---
+++ craft_common19_java_class_tsion/src/com/operation/PostFixandPreFix.java
@@ -0,0 +1,18 @@
+package com.operation;
+
+public class PostFixandPreFix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 12;
+System.out.println(a++);
+System.out.println(++a);
+System.out.println(a--);
+System.out.println(--a);
+
+for(int i =1; i<10; i++) {
+	
+}
+	}
+
+}
