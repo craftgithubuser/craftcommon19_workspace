f2731a0b65d41d93fa14c0031f2242fd9acc1dea
JGebru <103537344+JGebru@users.noreply.github.com>
2023-06-20 14:40:05 -0400

    Maybe!

---
+++ CraftCommon19_Java_Session/src/com/icraft/day1/Lesson1.java
@@ -0,0 +1,17 @@
+package com.icraft.day1;
+
+public class Lesson1 {
+
+	public static void main(String[] args) {
+		System.out.println("I Am Learning Java");
+		System.out.println("Hello Team");
+		System.out.println("I need to practice more");
+		System.out.println("Thank you for your help");
+		
+
+
+		
+
+	}
+
+}
