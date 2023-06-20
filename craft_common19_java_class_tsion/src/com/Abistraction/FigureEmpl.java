18fc724f32eb8864b52c20658f0af73d84f51ddb
Ketema19 <131938061+Ketema19@users.noreply.github.com>
2023-06-20 16:01:44 -0400

    abstraction exersice.

---
+++ craft_common19_java_class_tsion/src/com/Abistraction/FigureEmpl.java
@@ -0,0 +1,13 @@
+package com.Abistraction;
+
+public class FigureEmpl extends Figure {
+
+	@Override
+	public void calcArea(int length) {
+		// TODO Auto-generated method stub
+		int result = length *length;
+		System.out.println("Area of rectangle" + result);
+		
+	}
+
+}
