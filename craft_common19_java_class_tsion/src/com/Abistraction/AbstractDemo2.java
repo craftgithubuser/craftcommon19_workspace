ce6ba4c4956347132b86d7313f92fcb79f89ab66
Ketema19 <131938061+Ketema19@users.noreply.github.com>
2023-06-20 18:01:28 -0400

    new!

---
+++ craft_common19_java_class_tsion/src/com/Abistraction/AbstractDemo2.java
@@ -0,0 +1,13 @@
+package com.Abistraction;
+
+public class AbstractDemo2 extends AbstractDemo1{
+
+	@Override
+	public void callme() {
+		// TODO Auto-generated method stub
+		System.out.println("implemmentation of callme in abstractionDemo2");
+		
+	}
+	
+
+}
