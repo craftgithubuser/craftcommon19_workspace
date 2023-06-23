1337727cee4c09e90d4f8e5dc346633791cadd81
Ketema19 <131938061+Ketema19@users.noreply.github.com>
2023-06-22 23:58:41 -0400

    new exersice by maven.

---
+++ Maven_exrsice_One/src/test/java/com/icraft/CalculatoeImplTest.java
@@ -0,0 +1,14 @@
+package com.icraft;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatoeImplTest {
+
+	@Test
+	public void test() {
+		assertEquals(12, 12);
+	}
+
+}
